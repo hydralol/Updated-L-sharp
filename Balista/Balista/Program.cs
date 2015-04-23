2c851e487d64afc329f818163543d8a18c7d9e9e
hydralol <deathsama@hotmail.fr>
2015-04-24 00:07:01 +0300

    balista
    balista

---
+++ Balista/Balista/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+using SharpDX;
+using Color = System.Drawing.Color;
+
+namespace Balista
+{
+    internal class Program
+    {
+        /*
+         * This is my first assembly/script whatever you wanna call it written for LeagueSharp(L#).
+         * This can and will be improved as I progress to learn more about LeagueSharp.Common etc.
+         * I've made a few programs before and I always struggle with the most important thing: Performance.
+         * So if you think one of my assemblies/programs can be improved please contact me. :)
+        */
+
+        public static string BalistaVersion = "1.0.0.4";
+        public static Spell R;
+        private static Obj_AI_Hero Player;
+        public static Menu menu;
+
+
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static bool BlitzInGame()
+        {
+            return ObjectManager.Get<Obj_AI_Hero>().Any(h => h.IsAlly && !h.IsMe && h.ChampionName == "Blitzcrank");
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Player = ObjectManager.Player;
+
+            //Check to see if it is Kalista.
+            if (Player.ChampionName != "Kalista") return;
+
+            //Check if you have a Blitzfriend :)
+            if(!BlitzInGame()) return;
+
+            R = new Spell(SpellSlot.R, 1500);
+            R.SetSkillshot(0.50f, 1500, float.MaxValue, false, SkillshotType.SkillshotCircle);
+
+            menu = new Menu("Balista", "Balista", true);
+            {
+                menu.AddItem(new MenuItem("useToggle", "Toggle").SetValue(false));
+                menu.AddItem(new MenuItem("useOnComboKey", "Enabled").SetValue(new KeyBind(32, KeyBindType.Press)));
+            }
+            Menu targetMenu = new Menu("Target Selector", "Target Selector");
+            {
+                foreach (
+                    Obj_AI_Hero enem in
+                        ObjectManager.Get<Obj_AI_Hero>()
+                            .Where(enem => enem.IsValid && enem.IsEnemy))
+                {
+                    targetMenu.AddItem(new MenuItem("target"+enem.ChampionName, enem.ChampionName).SetValue(true));
+                }
+            }
+            Menu drawMenu = new Menu("Drawings", "Drawings");
+            {
+                drawMenu.AddItem(new MenuItem("minBRange", "Balista Min Range", true).SetValue(new Circle(false, Color.Chartreuse)));
+                drawMenu.AddItem(new MenuItem("maxBRange", "Balista Max Range", true).SetValue(new Circle(false, Color.Green)));
+            }
+            Menu misc = new Menu("Misc", "misc");
+            {
+                misc.AddItem(new MenuItem("minRange", "Min Range to Balista", true).SetValue(new Slider(700, 100, 1449)));
+                misc.AddItem(new MenuItem("maxRange", "Max Range to Balista", true).SetValue(new Slider(1500, 100, 1500)));
+                misc.AddItem(new MenuItem("usePackets", "Use Packets").SetValue(false));
+            }
+
+            menu.AddSubMenu(targetMenu);
+            menu.AddSubMenu(drawMenu);
+            menu.AddSubMenu(misc);
+            menu.AddToMainMenu();
+
+            Game.OnUpdate += Game_OnUpdate;
+            Drawing.OnDraw += Drawing_OnDraw;
+
+            //This is art.. =.='
+            Game.PrintChat("<font size='21'><b><font color='#f8c906'>B</font><font color='#00bce5'>alista:</font></b></font><font size='17'> v" + BalistaVersion + " <font color='#189108'>loaded!</font> - <font color='#cccccc'>Hydralolz</font></font>");
+        }
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (menu.Item("minBRange", true).GetValue<Circle>().Active)
+            Render.Circle.DrawCircle(Player.Position, menu.Item("minRange", true).GetValue<Slider>().Value, menu.Item("minBRange", true).GetValue<Circle>().Color, 3);
+            if (menu.Item("maxBRange", true).GetValue<Circle>().Active)
+                Render.Circle.DrawCircle(Player.Position, menu.Item("maxRange", true).GetValue<Slider>().Value, menu.Item("maxBRange", true).GetValue<Circle>().Color, 3);
+        }
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            if (Player.IsDead || !menu.Item("useToggle").GetValue<bool>() &&
+                !menu.Item("useOnComboKey").GetValue<KeyBind>().Active || !R.IsReady()) return;
+
+            var blitzfriend =
+                ObjectManager.Get<Obj_AI_Hero>()
+                    .SingleOrDefault(
+                        x =>
+                            x.IsAlly && Player.Distance(x.ServerPosition) < menu.Item("maxRange", true).GetValue<Slider>().Value &&
+                            Player.Distance(x.ServerPosition) >= menu.Item("minRange", true).GetValue<Slider>().Value &&
+                            x.ChampionName == "Blitzcrank");
+
+            if (blitzfriend == null)
+                return;
+
+
+            foreach (
+                Obj_AI_Hero enem in
+                    ObjectManager.Get<Obj_AI_Hero>()
+                        .Where(enem => enem.IsValid && enem.IsEnemy && enem.Distance(Player) <= 2450f)) //+950f is blitz Q range.
+            {
+                if (menu.Item("target" + enem.ChampionName).GetValue<bool>() && enem.Health > 200)
+                {
+                    if (enem.Buffs != null)
+                    {
+                        for (int i = 0; i < enem.Buffs.Count(); i++)
+                        {
+                            if (enem.Buffs[i].Name == "rocketgrab2" && enem.Buffs[i].IsActive)
+                            {
+                                if (R.IsReady())
+                                {
+                                    R.Cast(menu.Item("usePackets").GetValue<bool>());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
--- Balista/Balista/Properties/AssemblyInfo.cs
+++ Balista/Balista/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hydra AIO")]
+[assembly: AssemblyTitle("Balista")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Hydra AIO")]
+[assembly: AssemblyProduct("Balista")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4a6669b8-379a-4f1d-9ee8-055a1f0fbdb9")]
+[assembly: Guid("e47624f8-ee22-4987-9b7b-93f196789c47")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5.8")]
-[assembly: AssemblyFileVersion("1.0.5.8")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
